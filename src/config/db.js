b125e07e7f1fccf8a0e9d55a546bc83176092adb
csantander93 <casdesarrollo.2022@gmail.com>
2024-12-30 02:25:50 -0300

    conexion con mongo en railway exitosa

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/taskmanager"; 
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    // Usa la URI desde las variables de entorno
+    const mongoURI = process.env.MONGO_URL || "mongodb://localhost:27017/taskmanager";
+    console.log("MONGO_URL:", mongoURI);
+
+    await mongoose.connect(mongoURI); 
     console.log("Conexión exitosa a MongoDB");
   } catch (error) {
-    console.error("Error al conectar a MongoDB:", error);
-    process.exit(1); 
+    console.error("Error al conectar a MongoDB:", error.message);
+
+    // Intentar reconectar después de un tiempo
+    setTimeout(connectDB, 5000); // Reintentar la conexión en 5 segundos
   }
 };
 
