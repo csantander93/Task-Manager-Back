22e2274d060f5e970bab71c055f68cef1eb92208
csantander93 <casdesarrollo.2022@gmail.com>
2025-01-09 03:25:11 -0300

    se cambio la bd ya que la anterior cayo y no sepudo levantar

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // Usa la URI desde las variables de entorno
-    const mongoURI = process.env.MONGO_URL || "mongodb://localhost:27017/taskmanager";
+    const mongoURI = process.env.MONGO_URL;
     console.log("MONGO_URL:", mongoURI);
 
     await mongoose.connect(mongoURI); 
